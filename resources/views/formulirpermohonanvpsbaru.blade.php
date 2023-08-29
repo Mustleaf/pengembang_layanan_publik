1e65a1c9543a4d8f633bf8301e90be7fdf657d66
yolandsss29 <yolandaavita21mei@gmail.com>
2023-08-29 14:40:53 +0700

    a
    a

---
+++ resources/views/formulirpermohonanvpsbaru.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/css/formulirpermohonanvpsbaru.css">
+    <title>Formulir Permohonan VPS Baru</title>
+  </head>
+  <body>
+
+  <!-- navbar start -->
+  <nav class="navbar navbar-expand-lg navbar-default bg-default" style="background-color:white">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" style="color:#2B3674" href="#">Formulir</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="container-fluid">
+    <ul class="navbar-nav justify-content-end">
+    <a class="navbar-brand fw-bold" style="color:#2B3674" href="#">Server</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+        </li>
+        </form>
+        </div>
+        </nav>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<h2>Formulir Permohonan VPS Baru</h2>
+<div class="formulir-vps">
+
+<div></div>
+</div>
+<!-- navbar end  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
