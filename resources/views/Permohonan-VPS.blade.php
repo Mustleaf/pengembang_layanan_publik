bfff46c325bdb922c5b0806cb9ad99cf4f53287c
yolandsss29 <yolandaavita21mei@gmail.com>
2023-09-07 15:49:05 +0700

    tes

--- resources/views/Permohonan-VPS.blade.php
+++ resources/views/Permohonan-VPS.blade.php
@@ -13,91 +13,117 @@
 
 
                     <form>
-                        <h5 class="text-center text-decoration-underline">Formulir Permohonan VPS Baru</h5>
-                </div>
-                <div class="datapemohon">
-                    <h6>Data Pemohon</h6>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 mb-2">
-                        <div class="form-outline">
-                            <label class="form-label" for="">Nama Pemohon :</label>
-                            <input type="text" id="" class="form-control" />
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <div class="form-outline">
-                            <label class="form-label" for="">NIP :</label>
-                            <input type="text" id="" class="form-control" />
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <div class="form-outline">
-                            <label class="form-label" for="">Instansi :</label>
-                            <input type="text" id="" class="form-control" />
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-2">
-                        <div class="form-outline">
-                            <label class="form-label" for="">Tujuan Pembuatan VPS :</label>
-                            <input type="text" id="" class="form-control" />
-                        </div>
-                    </div>
-                    <h6>Spesifikasi VPS yang diinginkan</h6>
-                    <div class="row">
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">Prosesor :</label>
-                                <input type="text" id="" class="form-control" />
+                        <h5 class="text-center text-decoration-underline mb-5">Formulir Permohonan VPS Baru</h5>
+                        <h6>Data Pemohon</h6>
+                        <div class="row">
+                            <div class="col-md-6 mb-2">
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Nama Pemohon :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">Sistem Operasi :</label>
-                                <input type="text" id="" class="form-control" />
+                            <div class="col-md-6 mb-2">
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Instansi :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">Hardisk :</label>
-                                <input type="text" id="" class="form-control" />
+                            <div class="col-md-6 mb-2">
+                                <div class="form-outline">
+                                    <label class="form-label" for="">NIP :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">RAM :</label>
-                                <input type="text" id="" class="form-control" />
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">IP Address :</label>
-                                <input type="text" id="" class="form-control" />
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">SSH :</label>
-                                <input type="text" id="" class="form-control" />
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-2">
-                            <div class="form-outline">
-                                <label class="form-label" for="">Remote Desktop :</label>
-                                <input type="text" id="" class="form-control" />
+                            <div class="col-md-6 mb-2">
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Tujuan Pembuatan VPS :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
                             </div>
                         </div>
 
-                        <div class="row mt-3">
-                            <div class="col-md-10 mb-2">
+                        <div class="row mb-2">
+                            <div class="col-md-8 mt-3">
+                                <h6>Spesifikasi VPS yang diinginkan</h6>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Prosesor :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">IP Address :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Sistem Operasi :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 mt-3">
+
+                                <h6>Hardisk</h6>
+                                <input type="checkbox" name="hardisk[]" value="20 GB"> 20 GB<br>
+                                <input type="checkbox" name="hardisk[]" value="40 GB"> 40 GB<br>
+
+                                <h6>RAM</h6>
+                                <input type="checkbox" name="RAM[]" value="1 GB"> 1 GB<br>
+                                <input type="checkbox" name="RAM[]" value="2 GB"> 2 GB<br>
+
+                                <h6>Akun Akses Non Fisik</h6>
+                                <input type="checkbox" name="Akun Akses Non Fisik[]" value="SSH"> SSH<br>
+                                <input type="checkbox" name="Akun Akses Non Fisik[]" value="Remote Dekstop"> Remote Dekstop<br>
+                            </div>
+                        </div>
+
+                        <h7>Dengan ini saya menyatakan bahwa data di atas adalah benar, dan akan mematuhi peraturan atau tata tertib data center yang berlaku.</h7>
+
+                        <div class="row">
+                            <div class="col-md-6 mt-3">
+                                <h6>Mengetahui</h6>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Nama :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">NIP :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Jabatan :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mt-3">
+                                <h6>Pemohon</h6>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Nama :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">NIP :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                                <div class="form-outline">
+                                    <label class="form-label" for="">Jabatan :</label>
+                                    <input type="text" id="" class="form-control" />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+            <div class="row mt-3 mb-4  ">
+                            <div class="col-auto mr-auto  ">
                                 <div class="form-outline">
                                     <button type="button" class="btn btn-primary btn-lg">Submit</button>
+                                </div>
+                            </div>
+                            <div class="col-auto ">
+                                <div class="form-outline">
                                     <button type="button" class="btn btn-primary btn-lg">Print</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
 
             @endsection
