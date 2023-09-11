a9be2f783c9c4abb1ef6ccd84ff448fc69885cc3
phaulinanggita <phaulinanggita@gmail.com>
2023-09-11 10:22:21 +0700

    Update Multi-User.blade.php

--- resources/views/Multi-User.blade.php
+++ resources/views/Multi-User.blade.php
@@ -18,7 +18,8 @@
     <img src="assets/img/kop.jpg" class="img-fluid" alt="" />
   </div>
 
-  <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5 w-75 p-3" data-aos="fade-up">
+  <div class="container shadow p-3 bg-body-tertiary rounded">
+  <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-3 w-80 p-3" data-aos="fade-up">
     <div class="row align-items-center mb-5">
       <div class="card bg-glass">
         <div class="card-body px-4 py-5 px-md-5">
@@ -33,102 +34,101 @@
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <h1 class="fs-5 mb-2 mt-5">
+            <h1 class="fs-5 mb-2 mt-5 text-left">
               Saya yang bertanda tangan di bawah ini :
             </h1>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Nama Lengkap :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">NIP :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Pangkat/Golongan :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Jabatan :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <h1 class="fs-5 mb-2 mt-5">
+            <h1 class="fs-5 mb-2 mt-5 text-left">
               Dengan ini memberikan rekomendasi kepada :
             </h1>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Nama Lengkap :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">NIP :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">NIK :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Pangkat/Golongan :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Jabatan :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Instansi :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Unit Kerja :</label>
               <input type="email" id="" class="form-control" placeholder="" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Alamat Email :</label>
               <input type="email" id="" class="form-control" placeholder="………………@kalteng.go.id" />
             </div>
 
-            <h1 class="fs-5 mb-2 mt-2">
+            <h1 class="fs-5 mb-2 mt-2 text-left">
               Untuk melakukan pendaftaran sertifikat elektronik sekaligus menjadi pemegang sertifikat elektronik yang digunakan pada :
             </h1>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Sistem :</label>
               <input type="email" id="" class="form-control" placeholder="Persuratan" />
             </div>
 
-            <div class="form-outline mb-2">
+            <div class="form-outline mb-2 text-left">
               <label class="form-label" for="form3Example3">Kegunaan :</label>
               <input type="email" id="" class="form-control" placeholder="Tanda Tangan Elektronik" />
             </div>
 
-            <h1 class="fs-5 mb-2 mt-5">
+            <h1 class="fs-5 mb-2 mt-5 text-left">
               Demikian surat rekomendasi ini saya buat, agar dapat digunakan sebagaimana mestinya.
             </h1>
 
             <div class="row">
-              <div class="col-md-8 mb-2">
+              <div class="col-md-8 mb-2 text-left">
                 <div class="form-outline">
-                  <label class="form-label" for="form3Example1">Upload Files Tanda Tangan</label>
-                  <button type="submit" class="btn btn-primary btn-block btn-sm ms-2">
-                    Upload Here
-                  </button>
+                <label class="form-label" for="form3Example1">Upload Files Tanda Tangan</label>
+                  <input type="file" id="file"/>
+                  <label for="file"></label>     
                 </div>
               </div>
-            </div>
+           
 
             <!-- Submit button -->
             <button type="submit" class="btn btn-primary btn-block btn-lg mb-0 mt-2">
@@ -141,8 +141,10 @@
       </div>
     </div>
   </div>
+  </div>
 
-  <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5 w-75 p-3" data-aos="fade-up">
+
+  <div class="container px-4 py-5 px-md-5 text-left text-lg-start my-3 w-80 p-3" data-aos="fade-up">
     <div class="row align-items-center mb-5">
       <div class="card bg-glass">
         <div class="card-body px-4 py-5 px-md-5">
@@ -217,6 +219,7 @@
         </div>
       </div>
     </div>
+  </div>
   </div>
 
   @endsection
